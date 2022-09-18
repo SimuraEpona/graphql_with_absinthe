692cc6fccb5c960b50b2ceb92a53c0295d012e43
RUIJIE.HAO <epona@hey.com>
2022-09-18 18:40:44 +0900

    this should be the start?

--- mix.exs
+++ mix.exs
@@ -48,6 +48,10 @@ defmodule PlateSlate.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
+      {:absinthe, "~> 1.7.0"},
+      {:absinthe_plug, "~> 1.5"},
+      {:absinthe_phoenix, "~> 2.0.0"},
+      {:absinthe_relay, "~> 1.5.0"},
       {:plug_cowboy, "~> 2.5"}
     ]
   end
