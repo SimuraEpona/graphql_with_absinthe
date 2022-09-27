9051fe0577fd9aaaac80f63c33a88b89f3475426
RUIJIE.HAO <epona@hey.com>
2022-09-27 20:52:13 +0900

    basic login context

--- mix.exs
+++ mix.exs
@@ -36,6 +36,8 @@ defmodule PlateSlate.MixProject do
     [
       {:phoenix, "~> 1.6.12"},
       {:phoenix_ecto, "~> 4.4"},
+      {:comeonin_ecto_password, "~> 3.0.0"},
+      {:pbkdf2_elixir, "~> 1.0"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
